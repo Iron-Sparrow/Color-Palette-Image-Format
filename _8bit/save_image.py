84e5fc33f196cfbbd5fec9cd1d6096659d8b593e
Adam Kudelas <adamkokudelas@gmail.com>
2025-04-09 22:45:32 +0200

    Update README and requirements; enhance file format description and dependencies

--- _8bit/save_image.py
+++ _8bit/save_image.py
@@ -1,7 +1,10 @@
 import numpy as np
 from pathlib import Path
+from typing import Final, Union, Optional
 
-def save_image_to_8bit_color_palette_image_file_format(pixel_array:np.ndarray, output_image_path:None|Path=None, limit_palette_size: int | np.uint8 | np.uint16 | np.uint32 = None):
+
+def save_image_to_8bit_color_palette_image_file_format(pixel_array:np.ndarray, output_image_path:Optional[Path]=None, limit_palette_size: Optional[int | np.uint8 | np.uint16 | np.uint32] = None):
     """
-        ...
+
     """
+
