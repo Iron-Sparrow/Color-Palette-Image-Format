4e43d48199acdbcbb7ff4b9dd3cf718ed3b6e23e
Adam Kudelas <adamkokudelas@gmail.com>
2025-04-09 21:41:48 +0200

    Fix type hint for pixel_array parameter in save_image_to_8bit_color_palette_image_file_format function

--- _8bit/save_image.py
+++ _8bit/save_image.py
@@ -1,7 +1,7 @@
 import numpy as np
 from pathlib import Path
 
-def save_image_to_8bit_color_palette_image_file_format(pixel_array:np.ndarray[dtype], output_image_path:None|Path=None, limit_palette_size: int | np.uint8 | np.uint16 | np.uint32 = None):
+def save_image_to_8bit_color_palette_image_file_format(pixel_array:np.ndarray, output_image_path:None|Path=None, limit_palette_size: int | np.uint8 | np.uint16 | np.uint32 = None):
     """
         ...
     """
